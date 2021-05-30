1bed3a2291630c10b7b35204121731417bbb7a1f
AAron <82711833+Timeandtid@users.noreply.github.com>
2021-05-30 12:21:06 +0300

    Update AARO.js

--- AARO.js
+++ AARO.js
@@ -8,11 +8,11 @@ AARO.js a moudle importer for khan academy
 
 //Mian 
 
- var _S = document.createElement("script");
- _S.src = "https://www.khanacademy.org/api/internal/scratchpads/6119752722235392?callback=_m";
- document.body.appendChild(_S);
- var _s = sessionStorage,
- 	_c, _m = d => _s.setItem("_", d.revision.code);
- _c = _s.getItem `_`;
- eval(_c);
- _s.removeItem `_`;
+var _S = document.createElement("script");
+_S.src = "https://www.khanacademy.org/api/internal/scratchpads/" + pgid + "?callback=_m";
+document.body.appendChild(_S);
+var _s = sessionStorage,
+	_c, _m = d => _s.setItem("_", d.revision.code);
+_c = _s.getItem `_`;
+eval(_c);
+_s.removeItem `_`;
